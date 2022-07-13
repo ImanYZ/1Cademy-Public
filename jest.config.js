43bb8e2bac1cf9c70bc4997208ffb2a69c630f15
Xavier Perez <xap5xap@users.noreply.github.com>
2022-07-13 11:35:07 -0500

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,33 @@ const createJestConfig = nextJest({
   dir: "./"
 });
 
+function makeModuleNameMapper(srcPath, tsconfigPath) {
+  // Get paths from tsconfig
+  const { paths } = require(tsconfigPath).compilerOptions;
+
+  const aliases = {};
+
+  // Iterate over paths and convert them into moduleNameMapper format
+  Object.keys(paths).forEach(item => {
+    const key = item.replace("/*", "/(.*)");
+    const path = paths[item][0].replace("/*", "/$1");
+    aliases[key] = srcPath + "/" + path;
+  });
+  return aliases;
+}
+
+const TS_CONFIG_PATH = "./tsconfig.json";
+const SRC_PATH = "<rootDir>";
+
 const customJestConfig = {
+  roots: [SRC_PATH],
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect", "@testing-library/react", "<rootDir>/jest.setup.ts"],
-  moduleDirectories: ["node_modules", "<rootDir>/"],
+  moduleDirectories: ["node_modules"],
   testEnvironment: "jest-environment-jsdom",
   testMatch: ["**/__tests__/**/*.[jt]s?(x)"],
   collectCoverage: true,
-  collectCoverageFrom: ["components/**/*.{js,jsx,ts,tsx}", "pages/**/*.{js,jsx,ts,tsx}"]
+  collectCoverageFrom: ["components/**/*.{js,jsx,ts,tsx}", "pages/**/*.{js,jsx,ts,tsx}"],
+  moduleNameMapper: makeModuleNameMapper(SRC_PATH, TS_CONFIG_PATH)
 };
 
 module.exports = createJestConfig(customJestConfig);
