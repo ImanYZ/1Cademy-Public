4068c68c5141a06c73f3055f313c60f68331bfca
jimy-nicanor <dev.jimynicanor@gmail.com>
2022-09-02 14:29:20 -0500

    revert next.config.js change

--- next.config.js
+++ next.config.js
@@ -6,7 +6,4 @@ module.exports = {
     domains: ["firebasestorage.googleapis.com", "storage.googleapis.com", "node.1cademy.us", "1cademy.us"],
     minimumCacheTTL: 315360,
   },
-  eslint: {
-    dirs: ["pages", "utils"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-  },
 };
