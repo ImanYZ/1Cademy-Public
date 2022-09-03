301c7b37679ade7358ae21cb4ee8964021195490
Usama Sehezad <usamashzd99@gmail.com>
2022-09-04 01:00:33 +0500

    mockData folder brought back to fix the cypress test.

---
+++ testUtils/mockData/signup.data.ts
@@ -0,0 +1,111 @@
+export const signUpResponse = {
+  user: {
+    ltermDay: 0,
+    lterm: 0,
+    cnCorrects: 0,
+    cnWrongs: 0,
+    cnInst: 0,
+    cdCorrects: 0,
+    cdWrongs: 0,
+    cdInst: 0,
+    qCorrects: 0,
+    qWrongs: 0,
+    qInst: 0,
+    pCorrects: 0,
+    pWrongs: 0,
+    pInst: 0,
+    sCorrects: 0,
+    sWrongs: 0,
+    sInst: 0,
+    aCorrects: 0,
+    aWrongs: 0,
+    aInst: 0,
+    rfCorrects: 0,
+    rfWrongs: 0,
+    rfInst: 0,
+    nCorrects: 0,
+    nWrongs: 0,
+    nInst: 0,
+    mCorrects: 0,
+    mWrongs: 0,
+    mInst: 0,
+    iCorrects: 0,
+    iWrongs: 0,
+    iInst: 0,
+    positives: 0,
+    negatives: 0,
+    totalPoints: 0,
+    isAdmin: false,
+    iInstValue: 4,
+    deepA: 750,
+    ltermA: 1375,
+    iValue: 10,
+    tagId: "r98BjyFDCe4YyLA3U8ZE",
+    meanAInst: 84,
+    deepAInst: 210,
+    meanA: 300,
+    ltermMaxDay: 25,
+    deCourse: "SI691",
+    tag: "1Cademy",
+    deCredits: 3,
+    fName: "321321",
+    lName: "2133213",
+    imageUrl: "https://storage.googleapis.com/onecademy-1.appspot.com/ProfilePictures/no-img.png",
+    chooseUname: false,
+    lang: "English",
+    uname: "23123",
+    theme: "Dark",
+    background: "Color",
+    practicing: false,
+    createdAt: "2022-07-20T16:32:57.194Z"
+  }
+};
+
+export const signInWithPasswordResponse = {
+  kind: "identitytoolkit#VerifyPasswordResponse",
+  localId: "ax7p3NsPApXAwBtekkbrZpgdwPp1",
+  email: "two2@umich.edu",
+  displayName: "23123",
+  idToken:
+    "eyJhbGciOiJSUzI1NiIsImtpZCI6ImJmMWMyNzQzYTJhZmY3YmZmZDBmODRhODY0ZTljMjc4ZjMxYmM2NTQiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiMjMxMjMiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vb25lY2FkZW15LWRldiIsImF1ZCI6Im9uZWNhZGVteS1kZXYiLCJhdXRoX3RpbWUiOjE2NTgzMzQ3NzksInVzZXJfaWQiOiJheDdwM05zUEFwWEF3QnRla2ticlpwZ2R3UHAxIiwic3ViIjoiYXg3cDNOc1BBcFhBd0J0ZWtrYnJacGdkd1BwMSIsImlhdCI6MTY1ODMzNDc3OSwiZXhwIjoxNjU4MzM4Mzc5LCJlbWFpbCI6InR3bzJAdW1pY2guZWR1IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbInR3bzJAdW1pY2guZWR1Il19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.OpsLxfGhTV8y1EWWHCkLbXLudJbxs5carIZ51niu6ql5Rjo3F4mYjwdMfOOCtWrBRJVGjcl_vMJnTnxiotk0RJ_UgbUaLOuCjhfdw89qmnN9c8RQmfcoFOax_5Fg6clDWVrgEgrj6aSdWj60THZzVvZ2j-Aa8rew8jtP7vVKdaA3XYxOBd4mHFXtWmxCYsch5RIMasGrqn-usmp2qvcvVrEm6pA3nMlTlKuVKtuF9BlM6c_cWt6Fxte0NAlkK98HIbG7TRUScZ4F0FPQNBON9bnc-fCTwdey1Nq0vZaCJPPbmvX9VNQz_H5ckgCcCmFw4rzfvFxECGnvQbB5SWOYLA",
+  registered: true,
+  refreshToken:
+    "AOEOulb5meoLdAhxGcTmXgPyAQjQWF_FzmIt1Ohr0pPM0p2IxIvXN4K3hJgLl07SLYXMEY-k4n48iL7RTXjCU3nAOCKha3Qop_h3yR8WzeHOc9oqVH-CRd0xGxEQOUmkYgZ8qDDfikIeD-7E1x9TRGNeaw_gx3dgyDAV9nXr2pv8yj-U2u6cVsL7T_M6e4KgW6PksrAGp5tKEvXRKhzqGWKaooU4J4FRRg",
+  expiresIn: "3600"
+};
+
+export const lookupResponse = {
+  kind: "identitytoolkit#GetAccountInfoResponse",
+  users: [
+    {
+      localId: "ax7p3NsPApXAwBtekkbrZpgdwPp1",
+      email: "two2@umich.edu",
+      displayName: "23123",
+      passwordHash: "UkVEQUNURUQ=",
+      emailVerified: false,
+      passwordUpdatedAt: 1658334776557,
+      providerUserInfo: [
+        {
+          providerId: "password",
+          displayName: "23123",
+          federatedId: "two2@umich.edu",
+          email: "two2@umich.edu",
+          rawId: "two2@umich.edu"
+        }
+      ],
+      validSince: "1658334776",
+      disabled: false,
+      lastLoginAt: "1658334779696",
+      createdAt: "1658334776557",
+      lastRefreshAt: "2022-07-20T16:32:59.696Z"
+    }
+  ]
+};
+
+export const signUpEmailRegistered = {
+  errorMessage: "This email address is already in use"
+};
+
+export const signUpUsernameRegistered = {
+  errorMessage: "This username is already in use"
+};
