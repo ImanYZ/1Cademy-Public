98f661a1a432973abee3d0fdd66d22452aaae732
Haroon Waheed <haroon.waheed21@gmail.com>
2022-09-21 01:29:16 +0500

    Created unit test for getTypedCollections method

---
+++ testUtils/mockCollections/newsVersions.data.ts
@@ -0,0 +1,18 @@
+const collection = "newsVersions";
+
+const data: any[] = [
+  {
+    documentId: "1man",
+    award: false,
+    correct: true,
+    createdAt: "January 15, 2021 at 3:05:15 AM UTC+5",
+    updatedAt: "January 15, 2021 at 3:05:15 AM UTC+5",
+    user: "elijah-fox",
+    version: "ibdbxeC0PspeOpBlOCAP",
+    wrong: false,
+  },
+];
+
+const newsVersionsData = { data, collection };
+
+export default newsVersionsData;
---
+++ testUtils/mockCollections/userCodeVersions.data.ts
@@ -0,0 +1,18 @@
+const collection = "userCodeVersions";
+
+const data: any[] = [
+  {
+    documentId: "1man",
+    award: false,
+    correct: true,
+    createdAt: "January 15, 2021 at 3:05:15 AM UTC+5",
+    updatedAt: "January 15, 2021 at 3:05:15 AM UTC+5",
+    user: "elijah-fox",
+    version: "ibdbxeC0PspeOpBlOCAP",
+    wrong: false,
+  },
+];
+
+const userCodeVersionsData = { data, collection };
+
+export default userCodeVersionsData;
