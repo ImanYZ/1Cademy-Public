88dcdab0b73cbe816c6f322bd09249f2193327a2
Haroon Waheed <haroon.waheed21@gmail.com>
2022-10-11 21:31:09 +0500

    Create searchNodeInNotebook API test

---
+++ __tests__/pages/api/searchNodesInNotebook.test.ts
@@ -0,0 +1,63 @@
+import HttpMock from "node-mocks-http";
+import { TypesenseMock } from "testUtils/typesenseMocks";
+import NodeTSSchema from "testUtils/typesenseMocks/nodes.schema";
+
+import searchNodesInNotebookHandler from "../../../src/pages/api/searchNodesInNotebook";
+import { convertNodeToTypeSchema, createNode, getDefaultNode } from "../../../testUtils/fakers/node";
+import { createUser, getDefaultUser } from "../../../testUtils/fakers/user";
+import deleteAllUsers from "../../../testUtils/helpers/deleteAllUsers";
+import { MockData } from "../../../testUtils/mockCollections";
+
+describe("POST /api/searchNodesInNotebook", () => {
+  const users = [getDefaultUser({})];
+  const nodes = [
+    getDefaultNode({
+      admin: users[0],
+    }),
+  ];
+  users.push(
+    createUser({
+      tag: nodes[0],
+      sNode: nodes[0],
+    })
+  );
+
+  nodes.push(
+    createNode({
+      admin: users[0],
+      tags: [nodes[0]],
+      corrects: 1,
+    })
+  );
+
+  const usersCollection = new MockData(users, "users");
+  const nodesCollection = new MockData(nodes, "nodes");
+
+  const nodesTSCollection = new TypesenseMock(
+    NodeTSSchema,
+    nodes.map(node => convertNodeToTypeSchema(node)),
+    "nodes"
+  );
+  const collects = [usersCollection, nodesCollection, nodesTSCollection];
+  beforeEach(async () => {
+    await Promise.all(collects.map(collect => collect.populate()));
+  });
+  afterEach(async () => {
+    await deleteAllUsers();
+    await Promise.all(collects.map(collect => collect.clean()));
+  });
+
+  it("Should be able to search data from searchNodesInNotebook api with specific title", async () => {
+    const req: any = HttpMock.createRequest({
+      method: "POST",
+      body: { q: nodes[0].title },
+    });
+    const res: any = HttpMock.createResponse();
+    await searchNodesInNotebookHandler(req, res);
+    const { data, page } = JSON.parse(res._getData());
+    expect(page).toEqual(1);
+    data.map((result: any) => {
+      expect(true).toBe(result.title.includes(nodes[0].title));
+    });
+  });
+});
