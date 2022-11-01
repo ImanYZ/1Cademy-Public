048c9f09168373041ce46df87a42ad6433cad4e1
wal33dahmad <rana.waleed123@gmail.com>
2022-11-01 19:45:36 -0400

    fixed some bugs and issues

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3882,7 +3882,7 @@ const Dashboard = ({}: DashboardProps) => {
             />
           )}
           <MemoizedCommunityLeaderboard userTagId={user?.tagId ?? ""} pendingProposalsLoaded={pendingProposalsLoaded} />
-          {nodeBookState.selectedNode && (
+          {nodeBookState.selectedNode && !openSidebar && (
             <Tooltip title="Scroll to last Selected Node" placement="left">
               <IconButton
                 color="secondary"
@@ -3899,7 +3899,7 @@ const Dashboard = ({}: DashboardProps) => {
               </IconButton>
             </Tooltip>
           )}
-          {process.env.NODE_ENV === "development" && (
+          {process.env.NODE_ENV === "development" && !openSidebar && (
             <Tooltip
               title={"Watch geek data"}
               sx={{
