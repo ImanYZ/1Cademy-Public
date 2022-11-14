614ee0c67643e7f3a38287233ed8151f7bb4aac0
Haroon Waheed <haroon.waheed21@gmail.com>
2022-11-15 02:28:20 +0500

    improve useThemeChanger hook

--- src/hooks/useThemeChange.ts
+++ src/hooks/useThemeChange.ts
@@ -8,93 +8,26 @@ const useThemeChange = () => {
   const [{ user, settings }, { dispatch }] = useAuth();
 
   const changeAttr = useCallback(
-    (
-        attrName:
-          | "fName"
-          | "lName"
-          | "theme"
-          | "background"
-          | "chooseUname"
-          | "lang"
-          | "gender"
-          | "ethnicity"
-          | "country"
-          | "state"
-          | "city"
-          | "reason"
-          | "foundFrom"
-          | "birthDate"
-          | "view"
-      ) =>
-      async (newValue: any) => {
-        if (!user) return;
-        const userRef = doc(db, "users", user.uname);
-        await updateDoc(userRef, { [attrName]: newValue });
-        let userLogCollection = "";
-        switch (attrName) {
-          case "fName":
-            userLogCollection = "userFNameLog";
-            break;
-          case "lName":
-            userLogCollection = "userLNameLog";
-            break;
-          case "theme":
-            userLogCollection = "userThemeLog";
-            break;
-          case "background":
-            userLogCollection = "userBackgroundLog";
-            break;
-          case "chooseUname":
-            userLogCollection = "userChooseUnameLog";
-            break;
-          case "lang":
-            userLogCollection = "userLangLog";
-            break;
-          case "gender":
-            userLogCollection = "userGenderLog";
-            break;
-          case "ethnicity":
-            userLogCollection = "userEthnicityLog";
-            break;
-          case "country":
-            userLogCollection = "userCountryLog";
-            break;
-          case "state":
-            userLogCollection = "userStateLog";
-            break;
-          case "city":
-            userLogCollection = "userCityLog";
-            break;
-          case "reason":
-            userLogCollection = "userReasonLog";
-            break;
-          case "foundFrom":
-            userLogCollection = "userFoundFromLog";
-            break;
-          case "birthDate":
-            userLogCollection = "userBirthDayLog";
-            break;
-          case "view":
-            userLogCollection = "userViewLog";
-            break;
-          default:
-          // code block
-        }
+    () => async (newValue: any) => {
+      if (!user) return;
+      const userRef = doc(db, "users", user.uname);
+      await updateDoc(userRef, { ["theme"]: newValue });
+      let userLogCollection = "userThemeLog";
 
-        const userLogRef = doc(collection(db, userLogCollection));
-        await setDoc(userLogRef, {
-          uname: user.uname,
-          [attrName]: newValue,
-          createdAt: Timestamp.fromDate(new Date()),
-        });
-      },
+      const userLogRef = doc(collection(db, userLogCollection));
+      await setDoc(userLogRef, {
+        uname: user.uname,
+        ["theme"]: newValue,
+        createdAt: Timestamp.fromDate(new Date()),
+      });
+    },
     [db, user]
   );
   const handleThemeSwitch = useCallback(
     (event: any) => {
       event.preventDefault();
       const newTheme = settings.theme === "Dark" ? "Light" : "Dark";
-      changeAttr("theme")(newTheme);
+      changeAttr()(newTheme);
       // setTheme(newTheme);
       dispatch({ type: "setTheme", payload: newTheme });
     },
