3c7422c68d4e657517928e721770c072be1568c3
Artemides <edmpulasky@gmail.com>
2022-11-21 07:32:50 -0500

    bookmark paper item cursor to pointer

--- src/components/map/Sidebar/BookmarksList.tsx
+++ src/components/map/Sidebar/BookmarksList.tsx
@@ -72,7 +72,7 @@ export const BookmarksList = ({ openLinkedNode, bookmarks, updates }: BookmarksL
             // CHECK: I changed: node.id to node.userNodeId
             key={`node${node.userNodeId}`}
             onClick={() => openLinkedNode(node.node)}
-            sx={{ listStyle: "none", padding: "10px" /*  mx: "10px" */ }}
+            sx={{ listStyle: "none", padding: "10px", cursor: "pointer" }}
           >
             <div className="SidebarNodeTypeIcon" style={{ display: "flex", justifyContent: "space-between" }}>
               <NodeTypeIcon nodeType={node.nodeType} sx={{ fontSize: "16px" }} />
