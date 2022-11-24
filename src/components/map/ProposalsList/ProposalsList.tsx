f4a1ab915d06af86aa245f1184369c30ad7fad90
elVengador <dev.jimynicanor@gmail.com>
2022-11-24 09:48:23 -0500

    fix build error

--- src/components/map/ProposalsList/ProposalsList.tsx
+++ src/components/map/ProposalsList/ProposalsList.tsx
@@ -63,141 +63,158 @@ const ProposalsList = ({ username, ...props }: ProposalsListProps) => {
     return !isAdmin || proposal.proposer === username;
   };
 
-  return props.proposals.map((proposal: any, proposalIdx: number) => {
-    const proposalSummaries = proposalSummariesGenerator(proposal);
-    const isDisabled = shouldDisableButton(proposal, props.isAdmin, username);
+  return (
+    <>
+      {props.proposals.map((proposal: any, proposalIdx: number) => {
+        const proposalSummaries = proposalSummariesGenerator(proposal);
+        const isDisabled = shouldDisableButton(proposal, props.isAdmin, username);
 
-    if ((props.editHistory && proposal.accepted) || (!props.editHistory && !proposal.accepted)) {
-      if (props.openProposal === proposal.id) {
-        let adminTooltip = "# of 1Admins who have awarded this proposal.";
-        if (!props.isAdmin) {
-          adminTooltip += " You cannot give this proposal an award, because you are not the 1Admin of this node.";
-        } else {
-          if (proposal.proposer === username) {
-            adminTooltip += " You cannot give your own proposal an award!";
+        if ((props.editHistory && proposal.accepted) || (!props.editHistory && !proposal.accepted)) {
+          if (props.openProposal === proposal.id) {
+            let adminTooltip = "# of 1Admins who have awarded this proposal.";
+            if (!props.isAdmin) {
+              adminTooltip += " You cannot give this proposal an award, because you are not the 1Admin of this node.";
+            } else {
+              if (proposal.proposer === username) {
+                adminTooltip += " You cannot give your own proposal an award!";
+              } else {
+                adminTooltip +=
+                  " You're the 1Admin of this node. Click to give this proposal an award, if you find it exceptionally helpful.";
+              }
+            }
+            return (
+              <li className="collection-item avatar" key={`Proposal${proposal.id}`}>
+                <Paper elevation={3} sx={{ display: "flex", padding: "10px 20px", flexDirection: "column" }}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      gap: "5px",
+                    }}
+                  >
+                    <div className="title Time" style={{ fontSize: "12px" }}>
+                      {dayjs(proposal.createdAt).fromNow()}
+                    </div>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "5px",
+                      }}
+                    >
+                      <ContainedButton
+                        title="Click if you find this proposal Unhelpful."
+                        onClick={() => rateProposalClick(proposal, proposalIdx, false, true, false)}
+                      >
+                        <Box sx={{ display: "flex", alignItems: "center", gap: "4px", fill: "inherit" }}>
+                          <CloseIcon
+                            fontSize="inherit"
+                            sx={{ fill: proposal.wrong ? "rgb(255, 29, 29)" : "inherit" }}
+                          />
+                          <span style={{ color: "inherit" }}>{shortenNumber(proposal.wrongs, 2, false)}</span>
+                        </Box>
+                      </ContainedButton>
+
+                      <ContainedButton
+                        title="Click if you find this proposal helpful."
+                        onClick={() => rateProposalClick(proposal, proposalIdx, true, false, false)}
+                      >
+                        <Box sx={{ display: "flex", alignItems: "center", gap: "4px", fill: "inherit" }}>
+                          <DoneIcon
+                            fontSize="inherit"
+                            sx={{ fill: proposal.correct ? "rgb(0, 211, 105)" : "inherit" }}
+                          />
+                          <span style={{ color: "inherit" }}>{shortenNumber(proposal.corrects, 2, false)}</span>
+                        </Box>
+                      </ContainedButton>
+
+                      <ContainedButton
+                        title={adminTooltip}
+                        onClick={() => {
+                          !props.isAdmin || proposal.proposer === username
+                            ? false
+                            : rateProposalClick(proposal, proposalIdx, false, false, true);
+                        }}
+                        disabled={isDisabled}
+                      >
+                        <Box sx={{ display: "flex", alignItems: "center", gap: "4px", fill: "inherit" }}>
+                          <GradeIcon
+                            fontSize="inherit"
+                            sx={{ fill: proposal.award ? "rgb(255, 166, 0)" : "inherit" }}
+                          />
+                          <span style={{ color: "inherit" }}>{shortenNumber(proposal.awards, 2, false)}</span>
+                        </Box>
+                      </ContainedButton>
+
+                      {!proposal.accepted && proposal.proposer === username && (
+                        <ContainedButton
+                          title={"Delete your proposal"}
+                          onClick={() => deleteProposalClick(proposal, proposalIdx)}
+                        >
+                          <Box
+                            sx={{ display: "flex", alignItems: "center", gap: "4px", fill: "inherit", paddingY: "5px" }}
+                          >
+                            <DeleteForeverIcon fontSize="inherit" sx={{ fill: "inherit" }} />
+                          </Box>
+                        </ContainedButton>
+                      )}
+                    </Box>
+                  </Box>
+                  <Box sx={{ display: "flex", flexDirection: "column", flexGrow: "1" }}>
+                    <div className="title Time" style={{ fontSize: "12px" }}>
+                      <div className="ProposalTitle" style={{ fontSize: "16px", fontWeight: "400" }}>
+                        {proposal.title}
+                      </div>
+                    </div>
+                    <div className="ProposalTitle">
+                      {proposalSummaries.length > 0 ? (
+                        proposalSummaries.map((prSummary: any, sIdx: number) => {
+                          return (
+                            <Box
+                              component="p"
+                              sx={{
+                                margin: "0",
+                                color: theme =>
+                                  theme.palette.mode === "light"
+                                    ? theme.palette.common.black
+                                    : theme.palette.common.white,
+                              }}
+                              key={"Summary" + proposal.id + sIdx}
+                            >
+                              {prSummary}
+                            </Box>
+                          );
+                        })
+                      ) : (
+                        <Editor label="" readOnly value={proposal.summary} setValue={() => {}}></Editor>
+                      )}
+                    </div>
+                    <div className="ProposalBody">
+                      <Editor label="" readOnly value={proposal.proposal} setValue={() => {}}></Editor>
+                    </div>
+                  </Box>
+                </Paper>
+              </li>
+            );
           } else {
-            adminTooltip +=
-              " You're the 1Admin of this node. Click to give this proposal an award, if you find it exceptionally helpful.";
+            // THIS Show pending proposal and approved proposals
+            return (
+              <Box key={proposal.id} sx={{ display: "flex", flexDirection: "column", gap: "4px" }}>
+                <ProposalItem
+                  key={proposal.id}
+                  proposal={proposal}
+                  selectProposal={props.selectProposal}
+                  proposalSummaries={proposalSummaries}
+                  shouldSelectProposal={true}
+                  showTitle={true}
+                />
+              </Box>
+            );
           }
         }
-        return (
-          <li className="collection-item avatar" key={`Proposal${proposal.id}`}>
-            <Paper elevation={3} sx={{ display: "flex", padding: "10px 20px", flexDirection: "column" }}>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  gap: "5px",
-                }}
-              >
-                <div className="title Time" style={{ fontSize: "12px" }}>
-                  {dayjs(proposal.createdAt).fromNow()}
-                </div>
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    gap: "5px",
-                  }}
-                >
-                  <ContainedButton
-                    title="Click if you find this proposal Unhelpful."
-                    onClick={() => rateProposalClick(proposal, proposalIdx, false, true, false)}
-                  >
-                    <Box sx={{ display: "flex", alignItems: "center", gap: "4px", fill: "inherit" }}>
-                      <CloseIcon fontSize="inherit" sx={{ fill: proposal.wrong ? "rgb(255, 29, 29)" : "inherit" }} />
-                      <span style={{ color: "inherit" }}>{shortenNumber(proposal.wrongs, 2, false)}</span>
-                    </Box>
-                  </ContainedButton>
-
-                  <ContainedButton
-                    title="Click if you find this proposal helpful."
-                    onClick={() => rateProposalClick(proposal, proposalIdx, true, false, false)}
-                  >
-                    <Box sx={{ display: "flex", alignItems: "center", gap: "4px", fill: "inherit" }}>
-                      <DoneIcon fontSize="inherit" sx={{ fill: proposal.correct ? "rgb(0, 211, 105)" : "inherit" }} />
-                      <span style={{ color: "inherit" }}>{shortenNumber(proposal.corrects, 2, false)}</span>
-                    </Box>
-                  </ContainedButton>
-
-                  <ContainedButton
-                    title={adminTooltip}
-                    onClick={() => {
-                      !props.isAdmin || proposal.proposer === username
-                        ? false
-                        : rateProposalClick(proposal, proposalIdx, false, false, true);
-                    }}
-                    disabled={isDisabled}
-                  >
-                    <Box sx={{ display: "flex", alignItems: "center", gap: "4px", fill: "inherit" }}>
-                      <GradeIcon fontSize="inherit" sx={{ fill: proposal.award ? "rgb(255, 166, 0)" : "inherit" }} />
-                      <span style={{ color: "inherit" }}>{shortenNumber(proposal.awards, 2, false)}</span>
-                    </Box>
-                  </ContainedButton>
-
-                  {!proposal.accepted && proposal.proposer === username && (
-                    <ContainedButton
-                      title={"Delete your proposal"}
-                      onClick={() => deleteProposalClick(proposal, proposalIdx)}
-                    >
-                      <Box sx={{ display: "flex", alignItems: "center", gap: "4px", fill: "inherit", paddingY: "5px" }}>
-                        <DeleteForeverIcon fontSize="inherit" sx={{ fill: "inherit" }} />
-                      </Box>
-                    </ContainedButton>
-                  )}
-                </Box>
-              </Box>
-              <Box sx={{ display: "flex", flexDirection: "column", flexGrow: "1" }}>
-                <div className="title Time" style={{ fontSize: "12px" }}>
-                  <div className="ProposalTitle" style={{ fontSize: "16px", fontWeight: "400" }}>
-                    {proposal.title}
-                  </div>
-                </div>
-                <div className="ProposalTitle">
-                  {proposalSummaries.length > 0 ? (
-                    proposalSummaries.map((prSummary: any, sIdx: number) => {
-                      return (
-                        <Box
-                          component="p"
-                          sx={{
-                            margin: "0",
-                            color: theme =>
-                              theme.palette.mode === "light" ? theme.palette.common.black : theme.palette.common.white,
-                          }}
-                          key={"Summary" + proposal.id + sIdx}
-                        >
-                          {prSummary}
-                        </Box>
-                      );
-                    })
-                  ) : (
-                    <Editor label="" readOnly value={proposal.summary} setValue={() => {}}></Editor>
-                  )}
-                </div>
-                <div className="ProposalBody">
-                  <Editor label="" readOnly value={proposal.proposal} setValue={() => {}}></Editor>
-                </div>
-              </Box>
-            </Paper>
-          </li>
-        );
-      } else {
-        // THIS Show pending proposal and approved proposals
-        return (
-          <Box key={proposal.id} sx={{ display: "flex", flexDirection: "column", gap: "4px" }}>
-            <ProposalItem
-              key={proposal.id}
-              proposal={proposal}
-              selectProposal={props.selectProposal}
-              proposalSummaries={proposalSummaries}
-              shouldSelectProposal={true}
-              showTitle={true}
-            />
-          </Box>
-        );
-      }
-    }
-  });
+      })}
+    </>
+  );
 };
 
 export default React.memo(ProposalsList);
