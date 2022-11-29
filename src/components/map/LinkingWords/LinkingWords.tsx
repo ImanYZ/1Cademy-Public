2cff2c658a938bf12030aa5ee421c15ecfdccb9b
Artemides <edmpulasky@gmail.com>
2022-11-29 18:55:42 -0500

    move link to existing child button into flex container

--- src/components/map/LinkingWords/LinkingWords.tsx
+++ src/components/map/LinkingWords/LinkingWords.tsx
@@ -460,24 +460,24 @@ const LinkingWords = (props: LinkingWordsProps) => {
                 nodeType={"Relation"}
                 visible={false}
               />
+              {props.editable &&
+                !props.isNew &&
+                nodeBookState.selectedNode === props.identifier &&
+                props.openPart === "LinkingWords" && (
+                  <MemoizedMetaButton
+                    onClick={choosingNewLinkedNode("Child")}
+                    tooltip="Link to an existing child node."
+                    tooltipPosition="right"
+                  >
+                    <>
+                      <span>Link to an existing Child node</span>
+                      <AddIcon sx={{ color: "#00E676", fontSize: "16px" }} />
+                      <ArrowForwardIcon sx={{ color: "#00E676", fontSize: "16px" }} />
+                    </>
+                  </MemoizedMetaButton>
+                )}
             </Box>
           )}
-          {props.editable &&
-            !props.isNew &&
-            nodeBookState.selectedNode === props.identifier &&
-            props.openPart === "LinkingWords" && (
-              <MemoizedMetaButton
-                onClick={choosingNewLinkedNode("Child")}
-                tooltip="Link to an existing child node."
-                tooltipPosition="right"
-              >
-                <>
-                  <span>Link to an existing Child node</span>
-                  <AddIcon sx={{ color: "#00E676", fontSize: "16px" }} />
-                  <ArrowForwardIcon sx={{ color: "#00E676", fontSize: "16px" }} />
-                </>
-              </MemoizedMetaButton>
-            )}
         </Box>
       </Box>
     </>
