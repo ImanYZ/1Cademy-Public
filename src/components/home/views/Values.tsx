46e6585d9ffc27ca0d3b32b94aeab1e7f941a720
Artemides <edmpulasky@gmail.com>
2023-01-17 12:37:38 -0500

    animate cards items in why section of 1cademy homepage

--- src/components/home/views/Values.tsx
+++ src/components/home/views/Values.tsx
@@ -2,12 +2,39 @@ import { Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
 import Box from "@mui/material/Box";
 import React from "react";
 
+import { useInView, UseInViewProps } from "@/hooks/useObserver";
+
 import { gray03 } from "../../../pages";
 import valuesItems from "./valuesItems";
+const observerOption: UseInViewProps = { options: { root: null, rootMargin: "0px", threshold: 0.5 } };
+
 const Values = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery("(max-width:600px)");
   const isTablet = useMediaQuery("(min-width:900px)");
+
+  const { ref: ref1, inViewOnce: inViewOnce1 } = useInView(observerOption);
+  const { ref: ref2, inViewOnce: inViewOnce2 } = useInView(observerOption);
+  const { ref: ref3, inViewOnce: inViewOnce3 } = useInView(observerOption);
+  const { ref: ref4, inViewOnce: inViewOnce4 } = useInView(observerOption);
+  const { ref: ref5, inViewOnce: inViewOnce5 } = useInView(observerOption);
+  const { ref: ref6, inViewOnce: inViewOnce6 } = useInView(observerOption);
+  const { ref: ref7, inViewOnce: inViewOnce7 } = useInView(observerOption);
+  const { ref: ref8, inViewOnce: inViewOnce8 } = useInView(observerOption);
+  const { ref: ref9, inViewOnce: inViewOnce9 } = useInView(observerOption);
+
+  const refs = [ref1, ref2, ref3, ref4, ref5, ref6, ref7, ref8, ref9];
+  const inViewOnces = [
+    inViewOnce1,
+    inViewOnce2,
+    inViewOnce3,
+    inViewOnce4,
+    inViewOnce5,
+    inViewOnce6,
+    inViewOnce7,
+    inViewOnce8,
+    inViewOnce9,
+  ];
 
   const getGrayColorText = () => (theme.palette.mode === "dark" ? gray03 : theme.palette.common.darkBackground2);
 
@@ -24,6 +51,7 @@ const Values = () => {
       <Stack direction={"column"} spacing={isMobile ? "60px" : "100px"}>
         {valuesItems.map((value, idx) => (
           <Stack
+            ref={refs[idx]}
             key={idx}
             direction={isMobile ? "column" : idx % 2 === 0 ? "row" : "row-reverse"}
             spacing={isMobile ? "0px" : "40px"}
@@ -41,10 +69,14 @@ const Values = () => {
                 alignItems: "center",
                 justifyContent: isMobile ? "center" : idx % 2 === 0 ? "flex-start" : "flex-end",
               }}
+              className={inViewOnces[idx] ? (idx % 2 === 0 ? "slide-left-to-right" : "slide-right-to-left") : "hide"}
             >
               <img alt={value.name} src={"/static/" + value.image} style={{ flex: 1, width: "100%" }} />
             </Box>
-            <Box sx={{ p: "10px", display: "flex", flexDirection: "column", justifyContent: "center" }}>
+            <Box
+              sx={{ p: "10px", display: "flex", flexDirection: "column", justifyContent: "center" }}
+              className={inViewOnces[idx] ? (idx % 2 !== 0 ? "slide-left-to-right" : "slide-right-to-left") : "hide"}
+            >
               <Typography
                 gutterBottom
                 variant="h3"
