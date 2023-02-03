706ac4d65c853d0c4d1ccf6f426b913ce56d69ce
elVengador <dev.jimynicanor@gmail.com>
2023-02-03 14:37:53 -0500

    finish apply component

--- src/components/home/sections/Join.tsx
+++ src/components/home/sections/Join.tsx
@@ -2,6 +2,8 @@ import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 
+import ROUTES from "../../../lib/utils/routes";
+
 const Join = () => {
   return (
     <Box
@@ -14,14 +16,16 @@ const Join = () => {
         textAlign: "center",
       }}
     >
-      <Typography component={"h2"} sx={{ fontSize: "30px", mb: "16px" }}>
+      <Typography component={"h2"} sx={{ fontSize: { xs: "24px", md: "30px" }, fontWeight: 600, mb: "16px" }}>
         Join our massive collaboration to create a lasting impact.
       </Typography>
-      <Typography sx={{ fontSize: "20px", mb: "32px", maxWidth: "768px" }}>
+      <Typography sx={{ fontSize: { xs: "18px", md: "20px" }, mb: "32px", maxWidth: "768px" }}>
         We are a large community of researchers, students, and instructors dedicated to enhancing the standards of
         research and education.
       </Typography>
-      <Button variant="contained">Apply to join</Button>
+      <Button href={ROUTES.apply} target="_blank" rel="noopener" variant="contained">
+        Apply to join
+      </Button>
     </Box>
   );
 };
