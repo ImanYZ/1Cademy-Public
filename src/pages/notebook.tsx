dead23d566680d2dd2297a7801528c3364d7ae36
Haroon Waheed <haroon.waheed21@gmail.com>
2023-03-27 11:30:47 +0500

    Redesign community leaderboard for desktop

--- src/pages/notebook.tsx
+++ src/pages/notebook.tsx
@@ -5745,6 +5745,9 @@ const Dashboard = ({}: DashboardProps) => {
                     color="secondary"
                     onClick={onScrollToLastNode}
                     disabled={!nodeBookState.selectedNode ? true : false}
+                    sx={{
+                      opacity: !nodeBookState.selectedNode ? 0.5 : undefined,
+                    }}
                   >
                     <MyLocationIcon sx={{ color: theme => (theme.palette.mode === "dark" ? "#CACACA" : "#667085") }} />
                   </IconButton>
@@ -5819,6 +5822,9 @@ const Dashboard = ({}: DashboardProps) => {
                       }
                     }}
                     disabled={!nodeBookState.selectedNode ? true : false}
+                    sx={{
+                      opacity: !nodeBookState.selectedNode ? 0.5 : undefined,
+                    }}
                   >
                     <NextImage
                       src={theme.palette.mode === "light" ? focusViewLogo : focusViewDarkLogo}
