3d6dc30fa0c1517fc4e136fdd0d3bb71aa21c0c8
Artemides <edmpulasky@gmail.com>
2023-04-27 16:49:08 -0500

    unify into snapshot semester retrieving

--- src/components/map/Dashboard.tsx
+++ src/components/map/Dashboard.tsx
@@ -1,7 +1,20 @@
 import { Box, SxProps, Theme } from "@mui/material";
-import { collection, doc, getDoc, getDocs, getFirestore, query, where } from "firebase/firestore";
-import React, { useEffect, useMemo, useState } from "react";
-import { Semester, SemesterStudentVoteStat } from "src/instructorsTypes";
+import {
+  collection,
+  doc,
+  DocumentData,
+  Firestore,
+  getDoc,
+  getDocs,
+  getFirestore,
+  onSnapshot,
+  Query,
+  query,
+  Unsubscribe,
+  where,
+} from "firebase/firestore";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { Instructor, Semester, SemesterStudentVoteStat } from "src/instructorsTypes";
 import { User } from "src/knowledgeTypes";
 import { ICourseTag } from "src/types/ICourse";
 
@@ -23,12 +36,112 @@ type DashboardProps = {
 export const Dashboard = ({ user, sx, onClose }: DashboardProps) => {
   const db = getFirestore();
 
+  // const [semesters, setSemesters] = useState<string[]>([]);
   const [allCourses, setAllCourses] = useState<CoursesResult>({});
+  const [allSemesters, setAllSemesters] = useState<Semester[]>([]);
+
+  const [, /* instructor */ setInstructor] = useState<Instructor | null>(null);
+
   const [selectedSemester, setSelectedSemester] = useState<string | null>(null);
   const [courses, setCourses] = useState<any[]>([]);
   const [selectedCourse, setSelectedCourse] = useState<string | null>(null);
   const [currentSemester, setCurrentSemester] = useState<ICourseTag | null>(null);
-  const [allSemesters, setAllSemesters] = useState<Semester[]>([]);
+
+  const [, /* isLoading */ setIsLoading] = useState(true);
+
+  const semesterByStudentSnapthot = useCallback(
+    (q: Query<DocumentData>) =>
+      onSnapshot(q, async snaphot => {
+        const docChanges = snaphot.docChanges();
+
+        const semestersStudent = docChanges.map(change => {
+          const semesterData: SemesterStudentVoteStat = change.doc.data() as SemesterStudentVoteStat;
+          return {
+            id: change.doc.id,
+            data: semesterData,
+          };
+        });
+        const semestersIds = semestersStudent.map(cur => cur.data.tagId);
+        const semesters = await getSemesterByIds(db, semestersIds);
+        const coursesResult = semesters.reduce((acu: CoursesResult, cur: Semester) => {
+          const tmpValues = acu[cur.title] ?? [];
+          return { ...acu, [cur.title]: [...tmpValues, `${cur.cTitle} ${cur.pTitle}`] };
+        }, {});
+
+        // const semester = allSemesters.map(cur => cur.title);
+
+        setAllSemesters(allSemesters);
+        setAllCourses(coursesResult);
+        // setSelectedSemester(semester[0]);
+      }),
+    [allSemesters, db]
+  );
+
+  const semestersByInstructorSnapshot = (q: Query<DocumentData>) =>
+    onSnapshot(
+      q,
+      async snapshot => {
+        const docChanges = snapshot.docChanges();
+
+        // setIsLoading(false);
+        // if (!docChanges.length) {
+        //   return null;
+        // }
+
+        const intructor = docChanges[0].doc.data() as Instructor;
+        setInstructor(intructor);
+        const allCourses = getCoursesByInstructor(intructor);
+        const semestersIds = Object.keys(allCourses);
+
+        const semesters = await getSemesterByIds(db, semestersIds);
+
+        // if (!newSemesters.length) {
+        //   router.push(ROUTES.instructorsSettings);
+        // }
+
+        // const lastSemester = newSemesters.slice(-1)[0];
+
+        // setSemesters(prevSemester => {
+        //   setSelectedSemester(selectedSemester => {
+        //     if (!selectedSemester) {
+        //       return newSemesters[0];
+        //     }
+        //     if (!prevSemester.includes(lastSemester)) {
+        //       return lastSemester;
+        //     }
+
+        //     return selectedSemester;
+        //   });
+        //   return newSemesters;
+        // });
+        setAllSemesters(semesters);
+        setAllCourses(allCourses);
+      },
+      (error: any) => {
+        console.error(error);
+        setIsLoading(false);
+      }
+    );
+
+  useEffect(() => {
+    if (!user) return;
+    let killSnapshot: Unsubscribe | null = null;
+    if (user.role === "INSTRUCTOR") {
+      const instructorsRef = collection(db, "instructors");
+      const q = query(instructorsRef, where("uname", "==", user.uname));
+      killSnapshot = semestersByInstructorSnapshot(q);
+    }
+
+    if (user.role === "STUDENT") {
+      const semestersRef = collection(db, "semesterStudentVoteStats");
+      const q = query(semestersRef, where("uname", "==", user.uname));
+      killSnapshot = semesterByStudentSnapthot(q);
+    }
+
+    return () => {
+      if (killSnapshot) killSnapshot();
+    };
+  }, [db, semesterByStudentSnapthot, user]);
 
   // const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
@@ -144,7 +257,27 @@ export const Dashboard = ({ user, sx, onClose }: DashboardProps) => {
     </Box>
   );
 };
+
+const getCoursesByInstructor = (instructor: Instructor): CoursesResult => {
+  return instructor.courses.reduce((acu: CoursesResult, cur) => {
+    const tmpValues = acu[cur.title] ?? [];
+    return { ...acu, [cur.title]: [...tmpValues, `${cur.cTitle} ${cur.pTitle || "- " + cur.uTitle}`] };
+  }, {});
+};
+
 const getCourseBySemester = (semester: string | undefined, courses: { [key: string]: string[] }): string[] => {
   if (!semester) return [];
   return courses[semester] ?? [];
+};
+
+const getSemesterByIds = async (db: Firestore, semesterIds: string[]) => {
+  // const semestersIds = semestersStudent.map(cur => cur.data.tagId);
+
+  const semestersDocsPromises = semesterIds.map((semesterId: string) => {
+    const nodeRef = doc(db, "semesters", semesterId);
+    return getDoc(nodeRef);
+  });
+  const semesterDocs = await Promise.all(semestersDocsPromises);
+  const allSemesters = semesterDocs.map(cur => cur.data()).flatMap(c => (c as Semester) || []);
+  return allSemesters;
 };
