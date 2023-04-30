90e6f43f5044ff2bf12e8512b187b70a43aa2e39
Ameer Hamza <r3alst@gmail.com>
2023-05-01 04:43:31 +0500

    signup test fixed

--- __tests__/pages/api/signup.test.ts
+++ __tests__/pages/api/signup.test.ts
@@ -43,6 +43,10 @@ describe("/signup", () => {
     new MockData([], "userNodes"),
     new MockData([], "userNodesLog"),
     new MockData([], "users"),
+    new MockData([], "notebooks"),
+    new MockData([], "practice"),
+    new MockData([], "practiceLog"),
+    new MockData([], "semesterStudentSankeys"),
     tagsData,
     creditsData,
   ];
@@ -265,6 +269,18 @@ describe("/signup as student", () => {
     semestersCollection,
     semesterStudentStatsCollection,
     semesterStudentVoteStatsCollection,
+    new MockData([], "bookmarkNums"),
+    new MockData([], "credits"),
+    new MockData([], "notificationNums"),
+    new MockData([], "pendingPropsNums"),
+    new MockData([], "reputations"),
+    new MockData([], "userNodes"),
+    new MockData([], "userNodesLog"),
+    new MockData([], "users"),
+    new MockData([], "notebooks"),
+    new MockData([], "practice"),
+    new MockData([], "practiceLog"),
+    new MockData([], "semesterStudentSankeys"),
   ];
   let semesterId: string = "";
 
