4beff7fce851f92c28713d99f19861e48c4ed214
Ameer Hamza <r3alst@gmail.com>
2023-05-03 23:23:24 +0500

    included original request message in response for assistant

--- src/pages/api/assistant.ts
+++ src/pages/api/assistant.ts
@@ -129,6 +129,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
       actions: assistantMessage.actions,
       nodes: assistantMessage.nodes,
       is404: assistantMessage.is404,
+      request: payload.message,
     } as IAssistantResponse);
   } catch (error) {
     console.error(error);
