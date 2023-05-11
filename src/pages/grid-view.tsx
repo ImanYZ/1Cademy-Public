b52af4775ac4b9f0a8b4bc951f76af2826309004
elVengador <dev.jimynicanor@gmail.com>
2023-05-11 16:55:42 -0500

    detect height to draw nodes on correct positions

--- src/pages/grid-view.tsx
+++ src/pages/grid-view.tsx
@@ -1,20 +1,28 @@
 import { Box, SxProps, Theme, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-type Node = { id: string; content: string };
+type Node = { id: string; content: string; top: number; left: number };
+type NodePosition = "PARENTS" | "NODES" | "CHILDREN" | "SIBLINGS";
 
 type NodeGrid = {
-  node: Node;
-  parents: Node[];
-  children: Node[];
-  siblings: Node[];
+  center: { [key: string]: Node };
+  parents: { [key: string]: Node };
+  children: { [key: string]: Node };
+  siblings: { [key: string]: Node };
 };
 
 const GridView = () => {
-  const getPositionY = (lenght: number, idx: number, height: number) => {
-    const OFFSET = 100;
-    return idx * height + OFFSET;
+  // const getPositionY = (lenght: number, idx: number, height: number) => {
+  //   const OFFSET = 100;
+  //   return idx * height + OFFSET;
+  // };
+
+  const onChangeHeight = (id: string, height: number, type: "PARENT") => {
+    console.log({ id, height, type });
   };
+
+  const parents = Object.keys(NODE_GRID.parents).reduce((acu: Node[], cur) => [...acu, NODE_GRID.parents[cur]], []);
+
   return (
     <Box
       sx={{
@@ -26,22 +34,25 @@ const GridView = () => {
         position: "relative",
       }}
     >
-      <Node
+      {/* <Node
         id={NODE_GRID.node.id}
         content={NODE_GRID.node.content}
         sx={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%,-50%)" }}
-      />
+        changeNodeHight={onChangeHeight}
+      /> */}
       {/* parents */}
-      {NODE_GRID.parents.map((cur, idx, src) => (
+      {parents.map(cur => (
         <Node
           key={cur.id}
           id={cur.id}
           content={cur.content}
+          changeNodeHight={onChangeHeight}
+          position={"PARENTS"}
           sx={{
             position: "absolute",
-            top: getPositionY(src.length, idx, 200),
-            left: "20%",
-            transform: "translate(-50%,-50%)",
+            top: `${cur.top}px`,
+            left: `${cur.left}px`,
+            // transform: "translate(-50%,-50%)",
           }}
         />
       ))}
@@ -51,75 +62,137 @@ const GridView = () => {
 
 export default GridView;
 
-type NodeProps = { id: string; content: string; sx?: SxProps<Theme> };
-const Node = ({ id, content, sx }: NodeProps) => {
+type NodeProps = {
+  id: string;
+  content: string;
+  changeNodeHight: (id: string, height: number, position: NodePosition) => void;
+  position: NodePosition;
+  sx?: SxProps<Theme>;
+};
+const Node = ({ id, content, changeNodeHight, sx }: NodeProps) => {
+  const nodeRef = useRef<HTMLDivElement>(null);
+  const observer = useRef<ResizeObserver | null>(null);
+  const previousHeightRef = useRef<number>(0);
+  const previousTopRef = useRef<string>("0px");
+
+  useEffect(() => {
+    observer.current = new ResizeObserver(entries => {
+      try {
+        const { blockSize } = entries[0].borderBoxSize[0];
+        const topPosition = (entries[0].target as any)?.style?.top;
+        const isSimilar = blockSize === previousHeightRef.current;
+        previousHeightRef.current = blockSize;
+        previousTopRef.current = topPosition;
+        if (isSimilar) return;
+
+        changeNodeHight(id, blockSize);
+      } catch (err) {
+        console.warn("invalid entry", err);
+      }
+    });
+
+    if (nodeRef.current) {
+      observer.current.observe(nodeRef.current);
+    }
+
+    return () => {
+      if (!observer.current) return;
+      return observer.current.disconnect();
+    };
+  }, [changeNodeHight, id]);
+
   return (
-    <Box sx={{ width: "300px", backgroundColor: "#eee", border: "solid 2px orange", p: "10px", ...sx }}>
+    <Box ref={nodeRef} sx={{ width: "300px", backgroundColor: "#eee", border: "solid 2px orange", p: "10px", ...sx }}>
       <Typography sx={{ fontSize: "24px", color: "#123" }}>{id}</Typography>
       <Typography sx={{ color: "#123" }}>{content}</Typography>
     </Box>
   );
 };
+
 const NODE_GRID: NodeGrid = {
-  node: {
-    id: "10",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem culpa dolor unde at earum! Voluptatum provident, cupiditate ea officia debitis obcaecati, fugit odit architecto nihil delectus doloremque. Autem, commodi veritatis.",
+  center: {
+    "01": {
+      id: "10",
+      top: 0,
+      left: 0,
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem culpa dolor unde at earum! Voluptatum provident, cupiditate ea officia debitis obcaecati, fugit odit architecto nihil delectus doloremque. Autem, commodi veritatis.",
+    },
   },
-  parents: [
-    {
+  parents: {
+    "00": {
       id: "00",
+      top: 0,
+      left: 0,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem culpa dolor unde at earum! Voluptatum provident, cupiditate ea officia debitis obcaecati, fugit odit architecto nihil delectus doloremque. Autem, commodi veritatis.",
     },
-    {
+    "01": {
       id: "01",
+      top: 0,
+      left: 0,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem culpa dolor unde at earum! Voluptatum provident, cupiditate ea officia debitis obcaecati, fugit odit architecto nihil delectus doloremque. Autem, commodi veritatis.",
     },
-  ],
-  children: [
-    {
+  },
+  children: {
+    "11": {
       id: "11",
+      top: 0,
+      left: 0,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem culpa dolor unde at earum! Voluptatum provident, cupiditate ea officia debitis obcaecati, fugit odit architecto nihil delectus doloremque. Autem, commodi veritatis.",
     },
-    {
+    "12": {
       id: "12",
+      top: 0,
+      left: 0,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem culpa dolor unde at earum! Voluptatum provident, cupiditate ea officia debitis obcaecati, fugit odit architecto nihil delectus doloremque. Autem, commodi veritatis.",
     },
-    {
+    "13": {
       id: "13",
+      top: 0,
+      left: 0,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem culpa dolor unde at earum! Voluptatum provident, cupiditate ea officia debitis obcaecati, fugit odit architecto nihil delectus doloremque. Autem, commodi veritatis.",
     },
-  ],
-  siblings: [
-    {
+  },
+  siblings: {
+    "10a": {
       id: "10a",
+      top: 0,
+      left: 0,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem culpa dolor unde at earum! Voluptatum provident, cupiditate ea officia debitis obcaecati, fugit odit architecto nihil delectus doloremque. Autem, commodi veritatis.",
     },
-    {
+    "10b": {
       id: "10b",
+      top: 0,
+      left: 0,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem culpa dolor unde at earum! Voluptatum provident, cupiditate ea officia debitis obcaecati, fugit odit architecto nihil delectus doloremque. Autem, commodi veritatis.",
     },
-    {
+    "00a": {
       id: "00a",
+      top: 0,
+      left: 0,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem culpa dolor unde at earum! Voluptatum provident, cupiditate ea officia debitis obcaecati, fugit odit architecto nihil delectus doloremque. Autem, commodi veritatis.",
     },
-    {
+    "00b": {
       id: "00b",
+      top: 0,
+      left: 0,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem culpa dolor unde at earum! Voluptatum provident, cupiditate ea officia debitis obcaecati, fugit odit architecto nihil delectus doloremque. Autem, commodi veritatis.",
     },
-    {
+    "00c": {
       id: "00c",
+      top: 0,
+      left: 0,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem culpa dolor unde at earum! Voluptatum provident, cupiditate ea officia debitis obcaecati, fugit odit architecto nihil delectus doloremque. Autem, commodi veritatis.",
     },
-  ],
+  },
 };
