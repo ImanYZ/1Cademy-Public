5b644ab6fa40e5d44fda637a9cba47f295806326
elVengador <dev.jimynicanor@gmail.com>
2023-06-01 18:52:39 -0500

    add unit test to validate diferenct locations of user in stack bar

--- __tests__/client/utils/dashboard.test.ts
+++ __tests__/client/utils/dashboard.test.ts
@@ -87,4 +87,76 @@ describe("should calculate student location on Stack bar", () => {
     expect(questions).toBe(4);
     expect(practices).toBe(4);
   });
+
+  it("Get user location from a user who has hight proposals, middle questions and lower practices points", () => {
+    const UNAME = "Shahbab-Ahmed";
+    const semesterConfig = {
+      nodeProposals: { numProposalPerDay: 10, numPoints: 1 },
+      questionProposals: { numQuestionsPerDay: 10, numPoints: 1 },
+      dailyPractice: { numQuestionsPerDay: 10, numPoints: 1 },
+    } as ISemester;
+
+    const semesterStudentVoteStat = [
+      {
+        uname: UNAME,
+        days: [
+          { questionProposals: 10, correctPractices: 2, proposals: 10 },
+          { questionProposals: 10, correctPractices: 2, proposals: 10 },
+          { questionProposals: 10, correctPractices: 2, proposals: 10 },
+          { questionProposals: 5, correctPractices: 2, proposals: 10 },
+          { questionProposals: 5, correctPractices: 2, proposals: 10 },
+        ],
+      },
+      {
+        uname: "ironhulk19",
+        days: [
+          { questionProposals: 0, correctPractices: 10, proposals: 5 },
+          { questionProposals: 0, correctPractices: 10, proposals: 5 },
+          { questionProposals: 0, correctPractices: 10, proposals: 5 },
+          { questionProposals: 0, correctPractices: 10, proposals: 5 },
+          { questionProposals: 0, correctPractices: 10, proposals: 5 },
+        ],
+      },
+      {
+        uname: "sarahlatto",
+        days: [
+          { questionProposals: 0, correctPractices: 10, proposals: 5 },
+          { questionProposals: 0, correctPractices: 10, proposals: 5 },
+          { questionProposals: 0, correctPractices: 10, proposals: 5 },
+          { questionProposals: 0, correctPractices: 10, proposals: 5 },
+          { questionProposals: 0, correctPractices: 10, proposals: 5 },
+        ],
+      },
+      {
+        uname: "metzlera",
+        days: [
+          { questionProposals: 10, correctPractices: 10, proposals: 5 },
+          { questionProposals: 10, correctPractices: 10, proposals: 5 },
+          { questionProposals: 10, correctPractices: 10, proposals: 5 },
+          { questionProposals: 10, correctPractices: 10, proposals: 5 },
+          { questionProposals: 10, correctPractices: 10, proposals: 5 },
+        ],
+      },
+      {
+        uname: "elizadh",
+        days: [
+          { questionProposals: 10, correctPractices: 10, proposals: 5 },
+          { questionProposals: 10, correctPractices: 10, proposals: 5 },
+          { questionProposals: 10, correctPractices: 10, proposals: 5 },
+          { questionProposals: 10, correctPractices: 10, proposals: 5 },
+          { questionProposals: 10, correctPractices: 10, proposals: 5 },
+        ],
+      },
+    ] as SemesterStudentVoteStat[];
+
+    const { proposals, questions, practices } = getStudentLocationOnStackBar(
+      semesterStudentVoteStat,
+      semesterConfig,
+      UNAME
+    );
+
+    expect(proposals).toBe(4);
+    expect(questions).toBe(2);
+    expect(practices).toBe(0);
+  });
 });
