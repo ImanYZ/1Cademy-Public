364c3601201c32d9057d1ae43d1e01585fb42b26
Sam ouhra <ouhrac@gmail.com>
2023-06-27 20:11:24 +0100

    make sure to update the usernode if it's visible

--- src/utils/getAllUserNodes.ts
+++ src/utils/getAllUserNodes.ts
@@ -5,6 +5,7 @@ import { DocumentData } from "firebase-admin/firestore";
 
 export const getAllUserNodes = async ({ nodeId, t, onlyVisible }: any) => {
   let userNodesQuery = db.collection("userNodes").where("node", "==", nodeId);
+  //removed this because we need to update the usernodes if the node if it's visible in a notebook or not
   // if (onlyVisible) {
   //   // TODO: update only user nodes which has the notebooks
   //   // userNodesQuery = userNodesQuery.where("visible", "==", true);
@@ -14,15 +15,17 @@ export const getAllUserNodes = async ({ nodeId, t, onlyVisible }: any) => {
   const userNodesDocs = await convertToTGet(userNodesQuery, t);
   const userNodesData: any[] = [];
   const userNodesRefs: any[] = [];
-  userNodesDocs.forEach((doc: DocumentData) => {
+
+  for (let doc of userNodesDocs.docs) {
     const userNodeData = doc.data() as IUserNode;
-
-    if (userNodeData.notebooks && userNodeData.notebooks.length > 0) {
-      userNodesData.push({ ...doc.data(), id: doc.id });
-      userNodesRefs.push(db.collection("userNodes").doc(doc.id));
+    if (onlyVisible) {
+      if ((!userNodeData.notebooks || !userNodeData.notebooks.length) && !userNodeData.visible) {
+        continue;
+      }
     }
-  });
-
+    userNodesData.push({ ...doc.data(), id: doc.id });
+    userNodesRefs.push(db.collection("userNodes").doc(doc.id));
+  }
   userNodesData.forEach(c => console.log("get userNode:", c.id));
   return { userNodesData, userNodesRefs };
 };
