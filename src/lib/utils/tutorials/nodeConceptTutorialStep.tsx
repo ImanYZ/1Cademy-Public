2d45a588b22dfeb369c1cc2980f755d65316a327
Iman Yeckehzaare <oneweb@m-c02d3145md6v.local>
2023-07-03 10:03:08 -0400

    Updated the interactive tutorial instructions.

--- src/lib/utils/tutorials/nodeConceptTutorialStep.tsx
+++ src/lib/utils/tutorials/nodeConceptTutorialStep.tsx
@@ -9,14 +9,14 @@ import { getBaseStepConfig } from "./tutorial.utils";
 
 const NODE_CONCEPT_STEPS_CONFIG: TutorialStepConfig[] = [
   {
-    title: "What is a Concept Node",
+    title: "What is a Concept Node?",
     description: (
       <Stack>
-        <MarkdownRender text={"This is a **Concept** node. It can be distinguished by this icon here."} />
+        <MarkdownRender text={"This is a **Concept** node. It can be distinguished by the following icon:"} />
         <NodeTypeIconButton sx={{ mb: "4px", alignSelf: "center" }}>
           <LocalLibraryIcon fontSize="small" />
         </NodeTypeIconButton>
-        <MarkdownRender text={"Concept nodes describe a single, discrete concept"} />
+        <MarkdownRender text={"Concept nodes describe a single, discrete concept."} />
       </Stack>
     ),
     isClickable: true,
@@ -30,13 +30,26 @@ const NODE_CONCEPT_STEPS_CONFIG: TutorialStepConfig[] = [
             "Concept and relation nodes are very common. While each Concept node defines/explains a unique concept, each Relation node explains the relationship between different Concept nodes, without defining/explaining any of them."
           }
         />
-        <Stack direction={"row"} justifyContent={"center"} alignItems={"center"} spacing="24px" mt="4px">
-          <NodeTypeIconButton>
-            <LocalLibraryIcon fontSize="small" />
-          </NodeTypeIconButton>
-          <NodeTypeIconButton>
-            <ShareIcon fontSize="small" />
-          </NodeTypeIconButton>
+        <Stack
+          direction="row"
+          flexWrap="wrap"
+          justifyContent={"space-around"}
+          alignItems="center"
+          spacing="25px"
+          sx={{ mt: "4px", fontSize: "13px" }}
+        >
+          <Stack alignItems={"center"}>
+            <NodeTypeIconButton>
+              <LocalLibraryIcon color="inherit" fontSize="small" />
+            </NodeTypeIconButton>
+            <Typography fontSize={"inherit"}>Concept</Typography>
+          </Stack>
+          <Stack alignItems={"center"}>
+            <NodeTypeIconButton>
+              <ShareIcon color="inherit" fontSize="small" />
+            </NodeTypeIconButton>
+            <Typography fontSize={"inherit"}>Relation</Typography>
+          </Stack>
         </Stack>
       </>
     ),
