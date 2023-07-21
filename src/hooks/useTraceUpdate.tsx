37089a903b8b0a8e4d6443ae753919cbaeaa7223
elVengador <dev.jimynicanor1001@gmail.com>
2023-07-20 19:20:18 -0500

    fix build errors

--- src/hooks/useTraceUpdate.tsx
+++ src/hooks/useTraceUpdate.tsx
@@ -6,7 +6,7 @@ import { useEffect, useRef } from "react";
 export function useTraceUpdate(props: any) {
   const prev = useRef(props);
   useEffect(() => {
-    const changedProps = Object.entries(props).reduce((ps, [k, v]) => {
+    const changedProps = Object.entries(props).reduce((ps: any, [k, v]) => {
       if (prev.current[k] !== v) {
         ps[k] = [prev.current[k], v];
       }
