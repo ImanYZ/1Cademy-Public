1b5a65f34e181ad42450132250cb323c65700abf
Sam ouhra <ouhrac@gmail.com>
2024-02-19 21:33:31 +0100

    modify the email send to alert in case of errors

--- cloud-functions/functions/src/actions/sentAlertEmail.ts
+++ cloud-functions/functions/src/actions/sentAlertEmail.ts
@@ -12,13 +12,13 @@ export const sentAlertEmail = async (logData: any) => {
   try {
     let details = "";
     for (let key in logData) {
-      details += `${key}: ${logData[key]}\n`;
+      details += `<li>${key}: ${logData[key]}</li>\n`;
     }
     const mailOptions = {
       from: process.env.EMAIL,
       to: "ouhrac@gmail.com",
       subject: `Error in 1cademy`,
-      html: `Details:\n\n${details}`,
+      html: `Details:\n\n<ul>${details}</ul>`,
     };
 
     transporter.sendMail(mailOptions, async (error: any, data: any) => {
