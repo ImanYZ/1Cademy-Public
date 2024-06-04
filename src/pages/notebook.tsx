c169d2e6389d91ff3ec6905010701d06ce563c60
Sam ouhra <ouhrac@gmail.com>
2024-06-04 15:16:42 +0100

    update the arguments for the getCollectionsQuery function

--- src/pages/notebook.tsx
+++ src/pages/notebook.tsx
@@ -1407,69 +1407,67 @@ const Notebook = ({}: NotebookProps) => {
 
     const versionsSnapshots: any[] = [];
     const versions: { [key: string]: any } = {};
-    const NODE_TYPES_ARRAY: NodeType[] = ["Concept", "Code", "Reference", "Relation", "Question", "Idea"];
-    for (let nodeType of NODE_TYPES_ARRAY) {
-      const { versionsColl, userVersionsColl } = getCollectionsQuery(db, nodeType);
-      if (!versionsColl || !userVersionsColl) continue;
+    // const NODE_TYPES_ARRAY: NodeType[] = ["Concept", "Code", "Reference", "Relation", "Question", "Idea"];
 
-      const versionsQuery = query(
-        versionsColl,
-        where("accepted", "==", false),
-        where("tagIds", "array-contains", user.tagId),
-        where("deleted", "==", false)
-      );
+    const { versionsColl, userVersionsColl } = getCollectionsQuery(db);
 
-      const versionsSnapshot = onSnapshot(versionsQuery, async snapshot => {
-        const docChanges = snapshot.docChanges();
-        if (docChanges.length > 0) {
-          for (let change of docChanges) {
-            const versionId = change.doc.id;
-            const versionData = change.doc.data();
-            if (change.type === "removed") {
-              delete versions[versionId];
-            }
-            if (change.type === "added" || change.type === "modified") {
+    const versionsQuery = query(
+      versionsColl,
+      where("accepted", "==", false),
+      where("tagIds", "array-contains", user.tagId),
+      where("deleted", "==", false)
+    );
+
+    const versionsSnapshot = onSnapshot(versionsQuery, async snapshot => {
+      const docChanges = snapshot.docChanges();
+      if (docChanges.length > 0) {
+        for (let change of docChanges) {
+          const versionId = change.doc.id;
+          const versionData = change.doc.data();
+          if (change.type === "removed") {
+            delete versions[versionId];
+          }
+          if (change.type === "added" || change.type === "modified") {
+            versions[versionId] = {
+              ...versionData,
+              id: versionId,
+              createdAt: versionData.createdAt.toDate(),
+              award: false,
+              correct: false,
+              wrong: false,
+            };
+            delete versions[versionId].deleted;
+            delete versions[versionId].updatedAt;
+
+            const q = query(
+              userVersionsColl,
+              where("version", "==", versionId),
+              where("user", "==", user?.uname),
+              limit(1)
+            );
+
+            const userVersionsDocs = await getDocs(q);
+
+            for (let userVersionsDoc of userVersionsDocs.docs) {
+              const userVersion = userVersionsDoc.data();
+              delete userVersion.version;
+              delete userVersion.updatedAt;
+              delete userVersion.createdAt;
+              delete userVersion.user;
               versions[versionId] = {
-                ...versionData,
-                id: versionId,
-                createdAt: versionData.createdAt.toDate(),
-                award: false,
-                correct: false,
-                wrong: false,
+                ...versions[versionId],
+                ...userVersion,
               };
-              delete versions[versionId].deleted;
-              delete versions[versionId].updatedAt;
-
-              const q = query(
-                userVersionsColl,
-                where("version", "==", versionId),
-                where("user", "==", user?.uname),
-                limit(1)
-              );
-
-              const userVersionsDocs = await getDocs(q);
-
-              for (let userVersionsDoc of userVersionsDocs.docs) {
-                const userVersion = userVersionsDoc.data();
-                delete userVersion.version;
-                delete userVersion.updatedAt;
-                delete userVersion.createdAt;
-                delete userVersion.user;
-                versions[versionId] = {
-                  ...versions[versionId],
-                  ...userVersion,
-                };
-              }
             }
           }
-
-          const pendingProposals = { ...versions };
-          const proposalsTemp = Object.values(pendingProposals);
-          setPendingProposalsNum(proposalsTemp.length);
         }
-      });
-      versionsSnapshots.push(versionsSnapshot);
-    }
+
+        const pendingProposals = { ...versions };
+        const proposalsTemp = Object.values(pendingProposals);
+        setPendingProposalsNum(proposalsTemp.length);
+      }
+    });
+    versionsSnapshots.push(versionsSnapshot);
 
     return () => {
       for (let vSnapshot of versionsSnapshots) {
