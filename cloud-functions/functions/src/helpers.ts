8c97d361f537c8ef7fa8743be2a6fe877e02bf17
Sam ouhra <ouhrac@gmail.com>
2024-06-09 22:48:29 +0100

    add the tracking hours page

--- cloud-functions/functions/src/helpers.ts
+++ cloud-functions/functions/src/helpers.ts
@@ -1,3 +1,4 @@
+import moment = require("moment");
 import { db, commitBatch, batchUpdate } from "./admin";
 
 export const signalFlashcardChanges = async (nodeId: string, type: string) => {
@@ -36,4 +37,76 @@ export const signalFlashcardChanges = async (nodeId: string, type: string) => {
   }
   await commitBatch();
 };
+const getListOfStudents = async () => {
+  const semestersDocs = await db.collection("semesters").where("trackingHours", "==", true).get();
+
+  const studentsMap: { [uname: string]: any } = {};
+  for (let semesterDoc of semestersDocs.docs) {
+    const semesterData = semesterDoc.data();
+    for (let student of semesterData.students) {
+      studentsMap[student.uname] = { ...student, semesterId: semesterDoc.id, semesterName: semesterData.title };
+    }
+  }
+  return studentsMap;
+};
+const getMinutesDiff = (createdAt: any, lastActionTime: any) => {
+  const _createdAt = createdAt.seconds(0).milliseconds(0);
+  const _lastActionTime = lastActionTime.seconds(0).milliseconds(0);
+  return _createdAt.diff(_lastActionTime, "minutes");
+};
+export const trackHours = async (data: any) => {
+  try {
+    const students: { [uname: string]: any } = await getListOfStudents();
+    if (!(data.doer in students)) {
+      return;
+    }
+    const createdAt = moment(data.createdAt.toDate());
+    const todayDate = moment().format("DD-MM-YYYY");
+
+    const trackHoursDayQuery = await db
+      .collection("trackHours")
+      .where("uname", "==", data.doer)
+      .where("day", "==", todayDate)
+      .get();
+
+    if (trackHoursDayQuery.docs.length > 0) {
+      const trackDoc = trackHoursDayQuery.docs[0];
+      const trackData = trackDoc.data();
+      console.log(trackData);
+      const lastActionTime = moment(trackData.lastActionTime.toDate());
+
+      // Calculate the difference in minutes using
+      console.log(lastActionTime);
+      const diffInMinutes = getMinutesDiff(createdAt, lastActionTime);
+      console.log({ diffInMinutes });
+
+      // Update minutes for each day in the range
+      if (diffInMinutes > 0 && diffInMinutes <= 5) {
+        trackData.totalMinutes += diffInMinutes;
+        trackData.trackedMinutes.push(createdAt);
+      }
+      if (diffInMinutes > 5) {
+        trackData.totalMinutes += 1;
+        trackData.trackedMinutes.push(createdAt);
+      }
+      trackData.lastActionTime = createdAt;
+      trackDoc.ref.update(trackData);
+    } else {
+      // If it's the first action, just initialize lastActionTime
+      const newData = {
+        day: todayDate,
+        totalMinutes: 1,
+        trackedMinutes: [data.createdAt],
+        ...students[data.doer],
+        lastActionTime: createdAt,
+        createdAt: new Date(),
+      };
+      const trackHoursRef = db.collection("trackHours").doc();
+      trackHoursRef.set(newData);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const roundNum = (num: any) => Number(Number.parseFloat(Number(num).toFixed(2)));
