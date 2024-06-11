beffc633a5c1aad46a7dfc55d5f0b71d89b5cf6e
Haroon Waheed <haroonwaheed21@gmail.com>
2024-06-12 03:14:29 +0500

    Fixed the issue of default avatar of users in chat

--- src/components/map/Sidebar/Chat/Common/GroupAvatar.tsx
+++ src/components/map/Sidebar/Chat/Common/GroupAvatar.tsx
@@ -1,5 +1,7 @@
-import { Avatar, AvatarGroup } from "@mui/material";
+import { Avatar, AvatarGroup, Box } from "@mui/material";
 import React from "react";
+
+import OptimizedAvatar2 from "@/components/OptimizedAvatar2";
 
 type GroupAvatarProps = {
   membersInfo: any;
@@ -15,7 +17,18 @@ const GroupAvatar = ({ membersInfo, max = 5, size = 30 }: GroupAvatarProps) => {
       total={Object.keys(membersInfo)?.length}
     >
       {Object.keys(membersInfo).map((member: any, index: number) => {
-        return <Avatar key={index} alt={membersInfo[member]?.fullname} src={membersInfo[member]?.imageUrl} />;
+        return (
+          <Avatar key={index} alt={membersInfo[member]?.fullname} src={membersInfo[member]?.imageUrl}>
+            <Box>
+              <OptimizedAvatar2
+                alt={membersInfo[member]?.fullname || ""}
+                imageUrl={membersInfo[member]?.imageUrl || ""}
+                size={0}
+                sx={{ border: "none" }}
+              />
+            </Box>
+          </Avatar>
+        );
       })}
     </AvatarGroup>
   );
